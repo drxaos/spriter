e58c9782e3b23871c682eb75392b2c17c0b14b40
xaos <vladimir.p.polyakov@gmail.com>
2016-08-24 20:35:01 +0500

    fix font

--- src/main/java/com/github/drxaos/spriter/Spriter.java
+++ src/main/java/com/github/drxaos/spriter/Spriter.java
@@ -585,7 +585,7 @@ public class Spriter extends JFrame implements Runnable {
     /**
      * Create new sprite prototype. It's invisible and has zero width and height.
      *
-     * @param image Original image of new sprite.
+     * @param image        Original image of new sprite.
      * @param imageCenterX Distance from left side to center of image.
      * @param imageCenterY Distance from top side to center of image.
      * @return new sprite prototype
@@ -601,11 +601,11 @@ public class Spriter extends JFrame implements Runnable {
     /**
      * Create new sprite prototype. It's invisible and has zero width and height.
      *
-     * @param image Original image of new sprite.
+     * @param image        Original image of new sprite.
      * @param imageCenterX Distance from left side to center of image.
      * @param imageCenterY Distance from top side to center of image.
-     * @param frameWidth Single animation frame wifth.
-     * @param frameHeight Single animation frame height.
+     * @param frameWidth   Single animation frame wifth.
+     * @param frameHeight  Single animation frame height.
      * @return new sprite prototype
      */
     public Sprite createSpriteProto(BufferedImage image, double imageCenterX, double imageCenterY, int frameWidth, int frameHeight) {
@@ -619,12 +619,12 @@ public class Spriter extends JFrame implements Runnable {
     /**
      * Create new font sprite prototype. It's invisible and has zero width and height.
      *
-     * @param image Original image of new sprite.
+     * @param image        Original image of new sprite.
      * @param imageCenterX Distance from left side to center of image.
      * @param imageCenterY Distance from top side to center of image.
-     * @param frameWidth Single animation frame wifth.
-     * @param frameHeight Single animation frame height.
-     * @param symbols Array of symbols values in font. Example "abc\ndef\ghi".
+     * @param frameWidth   Single animation frame wifth.
+     * @param frameHeight  Single animation frame height.
+     * @param symbols      Array of symbols values in font. Example "abc\ndef\ghi".
      * @return new sprite prototype
      */
     public Font createFont(BufferedImage image, double imageCenterX, double imageCenterY, int frameWidth, int frameHeight, String symbols) {
@@ -638,10 +638,10 @@ public class Spriter extends JFrame implements Runnable {
     /**
      * Create new sprite.
      *
-     * @param image Original image of new sprite.
+     * @param image        Original image of new sprite.
      * @param imageCenterX Distance from left side to center of image.
      * @param imageCenterY Distance from top side to center of image.
-     * @param objectWidth Rendering width.
+     * @param objectWidth  Rendering width.
      * @param objectHeight Rendering height.
      * @return new sprite
      */
@@ -658,10 +658,10 @@ public class Spriter extends JFrame implements Runnable {
      * <br/>
      * Rendering height will be proportional to width.
      *
-     * @param image Original image of new sprite.
+     * @param image        Original image of new sprite.
      * @param imageCenterX Distance from left side to center of image.
      * @param imageCenterY Distance from top side to center of image.
-     * @param objectWidth Rendering width.
+     * @param objectWidth  Rendering width.
      * @return new sprite
      */
     public Sprite createSprite(BufferedImage image, double imageCenterX, double imageCenterY, double objectWidth) {
@@ -675,12 +675,12 @@ public class Spriter extends JFrame implements Runnable {
     /**
      * Create new animated sprite.
      *
-     * @param image Original image of new sprite.
+     * @param image        Original image of new sprite.
      * @param imageCenterX Distance from left side to center of image.
      * @param imageCenterY Distance from top side to center of image.
-     * @param frameWidth Single animation frame wifth.
-     * @param frameHeight Single animation frame height.
-     * @param objectWidth Rendering width.
+     * @param frameWidth   Single animation frame wifth.
+     * @param frameHeight  Single animation frame height.
+     * @param objectWidth  Rendering width.
      * @param objectHeight Rendering height.
      * @return new sprite
      */
@@ -697,12 +697,12 @@ public class Spriter extends JFrame implements Runnable {
      * <br/>
      * Rendering height will be proportional to width.
      *
-     * @param image Original image of new sprite.
+     * @param image        Original image of new sprite.
      * @param imageCenterX Distance from left side to center of image.
      * @param imageCenterY Distance from top side to center of image.
-     * @param frameWidth Single animation frame wifth.
-     * @param frameHeight Single animation frame height.
-     * @param objectWidth Rendering width.
+     * @param frameWidth   Single animation frame wifth.
+     * @param frameHeight  Single animation frame height.
+     * @param objectWidth  Rendering width.
      * @return new sprite
      */
     public Sprite createSprite(BufferedImage image, double imageCenterX, double imageCenterY, int frameWidth, int frameHeight, double objectWidth) {
@@ -1152,12 +1152,6 @@ public class Spriter extends JFrame implements Runnable {
         @Override
         public Font setSquareSide(double wh) {
             super.setSquareSide(wh);
-            return this;
-        }
-
-        @Override
-        public Font clone() {
-            super.clone();
             return this;
         }
 
--- src/main/java/com/github/drxaos/spriter/examples/Text.java
+++ src/main/java/com/github/drxaos/spriter/examples/Text.java
@@ -4,6 +4,11 @@ import com.github.drxaos.spriter.Spriter;
 
 import javax.imageio.ImageIO;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class Text {
 
@@ -22,68 +27,86 @@ public class Text {
 
         Spriter.Font font = spriter.createFont(ImageIO.read(Animation.class.getResource("/font1.png")), 0, 0, 90, 100,
                 "?bcdef\nghijkl\nmnopqr\nstuvwx\nyz1234\n567890\n!a%$(&\n@+-).,\n=#_* ").setWidthProportional(0.25);
-        double x = -1, y = -1;
-        font.getChar('t').setPos(x, y).setVisible(true);
-        x += font.getWidth();
-        font.getChar('y').setPos(x, y).setVisible(true);
-        x += font.getWidth();
-        font.getChar('p').setPos(x, y).setVisible(true);
-        x += font.getWidth();
-        font.getChar('e').setPos(x, y).setVisible(true);
-        x += font.getWidth();
+        final AtomicInteger x = new AtomicInteger(0);
+        final AtomicInteger y = new AtomicInteger(0);
+        final ArrayList<Spriter.Sprite> text = new ArrayList<>();
+        Function<Character, Spriter.Sprite> type = (c) -> {
+            text.add(font.getChar(c).setPos(x.getAndIncrement() * font.getWidth() - 1, y.get() * font.getHeight() - 1).setVisible(true));
+            if (x.get() >= 8) {
+                x.set(0);
+                y.incrementAndGet();
+            }
+            return null;
+        };
+        Runnable backspace = () -> {
+            if (text.size() > 0) {
+                text.remove(text.size() - 1).remove();
+                int xVal = x.decrementAndGet();
+                if (xVal < 0) {
+                    x.set(7);
+                    y.decrementAndGet();
+                }
+            }
+        };
+        type.apply('t');
+        type.apply('y');
+        type.apply('p');
+        type.apply('e');
 
         Spriter.Control control = spriter.getControl();
 
         while (true) {
-            Integer key = control.getKeyPress();
-            if (key != null) {
-                char c = (char) (int) key;
-                if (!Character.isISOControl(c)) {
-                    if (control.isKeyDown(KeyEvent.VK_SHIFT)) {
-                        switch (c) {
-                            case '-':
-                                c = '_';
-                                break;
-                            case '=':
-                                c = '+';
-                                break;
-                            case '1':
-                                c = '!';
-                                break;
-                            case '2':
-                                c = '@';
-                                break;
-                            case '3':
-                                c = '#';
-                                break;
-                            case '4':
-                                c = '$';
-                                break;
-                            case '5':
-                                c = '%';
-                                break;
-                            case '6':
-                                c = '^';
-                                break;
-                            case '7':
-                                c = '&';
-                                break;
-                            case '8':
-                                c = '*';
-                                break;
-                            case '9':
-                                c = '(';
-                                break;
-                            case '0':
-                                c = ')';
-                                break;
-                        }
+            keypress:
+            {
+                Integer key = control.getKeyPress();
+                if (key != null) {
+                    if (key == KeyEvent.VK_BACK_SPACE) {
+                        backspace.run();
+                        break keypress;
                     }
-                    font.getChar(Character.toLowerCase(c)).setPos(x, y).setVisible(true);
-                    x += font.getWidth();
-                    if (x >= 0.99) {
-                        x = -1;
-                        y += font.getHeight();
+                    char c = (char) (int) key;
+                    if (!Character.isISOControl(c)) {
+                        if (control.isKeyDown(KeyEvent.VK_SHIFT)) {
+                            switch (c) {
+                                case '-':
+                                    c = '_';
+                                    break;
+                                case '=':
+                                    c = '+';
+                                    break;
+                                case '1':
+                                    c = '!';
+                                    break;
+                                case '2':
+                                    c = '@';
+                                    break;
+                                case '3':
+                                    c = '#';
+                                    break;
+                                case '4':
+                                    c = '$';
+                                    break;
+                                case '5':
+                                    c = '%';
+                                    break;
+                                case '6':
+                                    c = '^';
+                                    break;
+                                case '7':
+                                    c = '&';
+                                    break;
+                                case '8':
+                                    c = '*';
+                                    break;
+                                case '9':
+                                    c = '(';
+                                    break;
+                                case '0':
+                                    c = ')';
+                                    break;
+                            }
+                        }
+                        type.apply(Character.toLowerCase(c));
                     }
                 }
             }
